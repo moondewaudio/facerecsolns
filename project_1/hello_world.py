074b09d425e4d84bccb4e715ba2effbdf62097ae
Simon <simonfong6@gmail.com>
2018-06-21 00:20:20 -0700

    Added instructions for program

--- project_1/hello_world.py
+++ project_1/hello_world.py
@@ -1,6 +1,17 @@
-def main():
-	print("Hello World!")
-	return
+"""
+hello_world.py
 
-if __name__ == "__main__":
-	main()
+Simple Hello World program.
+
+ECE196 Face Recognition Project
+Author: Will Chen
+
+1. Write a Write a program that prints "Hello World!" and uses the main function convention.
+"""
+
+# TODO: Write a program that prints "Hello World!" and uses a main function.
+def main():
+    print("Hello World!")
+
+if(__name__ == '__main__'):
+    main()
