054448db59762cf66ddf2f9c4a20b3c06c488ef5
Simon <simonfong6@gmail.com>
2018-06-21 00:45:55 -0700

    Changed comment strucuture

--- project_1/create_network.py
+++ project_1/create_network.py
@@ -5,38 +5,60 @@ Author: W Chen
 Adapted from: https://keras.io/getting-started/functional-api-guide/
 
 Modify this code to write a LeNet with the following requirements:
-* Input dimensions: 32x32x1
-* C1: convolutional layer, output: 6 layers of 28x28 feature maps, filter size: 5x5,
-  strides: 1 both horizontally and vertically, activation function: sigmoid
-* S2: max pooling layer, output: 6 layers of 14x14 feature maps, pooling size: 2x2,
-  strides: 2 both horizontally and vertically
-* C3: convolutional layer, output: 16 layers of 28x28 feature maps, filter size: 5x5,
-  strides: 1 both horizontally and vertically, activation function: sigmoid
-* S4: max pooling layer, output: 16 layers of 28x28 feature maps, pooling size: 2x2,
-  strides: 2 both horizontally and vertically
-* C5: convolutional layer, output: 120 layers of 1x1 feature maps, filter size: 5x5, activation function: sigmoid
-* F6: fully connected layer, output 84-dimensional vector, activation function: tanh
-* F7: fully connected layer, output 10-dimensional vector, activation function: softmax
-
+* Input dimensions: 32x32x1 - Input
+* C1: Convolutional Layer - Conv2D
+    number of filters: 6
+    kernel size: 5x5
+    strides: 1 both horizontally and vertically (Set by default.)
+    activation function: sigmoid
+    output: 6 layers of 28x28 feature maps (Do not need to specify in function.)
+* S2: Max Pooling Layer - MaxPooling2D
+    pooling size: 2x2
+    strides: 2 both horizontally and vertically
+    output: 6 layers of 14x14 feature maps (Do not need to specify in function.)
+* C3: Convolutional Layer - Conv2D
+    number of filters: 16
+    kernel size: 5x5
+    strides: 1 both horizontally and vertically
+    activation function: sigmoid
+    output: 16 layers of 10x10 feature maps(Do not need to specify in function.)
+* S4: Max Pooling Layer - MaxPooling2D
+    pooling size: 2x2
+    strides: 2 both horizontally and vertically
+    output: 16 layers of 5x5 feature maps (Do not need to specify in function.)
+* C5: Convolutional Layer - Conv2D
+    number of filters: 120
+    kernel size: 5x5
+    strides: 1 both horizontally and vertically
+    activation function: sigmoid
+    output: 120 layers of 1x1 feature maps(Do not need to specify in function.)
+* F6: Fully Connected Layer - Dense
+    units: 84
+    activation function: tanh
+    output 84-dimensional vector (This is specified through units.)
+* F7: Fully Connected Layer - Dense
+    units: 10
+    activation function: softmax
+    output 10-dimensional vector (This is specified through units.)
 """
-
+# TODO: Import other layers as necessary. (Conv2D, MaxPooling2D)
 from keras.layers import Input, Dense, Conv2D, MaxPooling2D
 from keras.models import Model
 
-# This returns a tensor
+# TODO: Currently, sets input dimension to be 784x1. Change to 32x32x1
 inputs = Input(shape=(32,32,1))
-x = Conv2D(6,(5,5), activation='sigmoid')(inputs)
-x = MaxPooling2D((2,2),strides=(2,2))(x)
-x = Conv2D(16,(5,5), activation='sigmoid')(x)
-x = MaxPooling2D((2,2),strides=(2,2))(x)
-x = Conv2D(120,(5,5), activation='sigmoid')(x)
 
-# a layer instance is callable on a tensor, and returns a tensor
-x = Dense(84, activation='tanh')(x)
-predictions = Dense(10, activation='softmax')(x)
-# the Input layer and three Dense layers
+x = Conv2D(6,(5,5), activation='sigmoid')(inputs)   # Convolution
+x = MaxPooling2D((2,2),strides=(2,2))(x)            # Pooling
+x = Conv2D(16,(5,5), activation='sigmoid')(x)       # Convolution
+x = MaxPooling2D((2,2),strides=(2,2))(x)            # Pooling
+x = Conv2D(120,(5,5), activation='sigmoid')(x)      # Convolution
+x = Dense(84, activation='tanh')(x)                 # Fully connected layer
+predictions = Dense(10, activation='softmax')(x)    # Inference layer
+
+# This creates a model that includes the Input layer and three Dense layers
 model = Model(inputs=inputs, outputs=predictions)
 
-# print model architecture
+# Print model architecture
 model.summary()
 
