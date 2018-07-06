c3885832af06bca8623f7bf101e9fb0b4cc182eb
Simon <simonfong6@gmail.com>
2018-07-05 19:35:24 -0700

    Starter knn

---
+++ updates_not_deployed/knn/knn.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+knn.py
+
+Performs K nearest neighbors on some given data.
+
+Author: Simon Fong
+"""
+
+class KNN:
+    def __init__(self, k=1):
+        pass
+
+    def __del__(self):
+        """ Destructor to close everything. """
+        # When everything done, release the video capture object
+        pass  
+
+
+def _main(args):
+    pass
+    
+    
+if(__name__ == '__main__'):
+    import argparse
+    parser = argparse.ArgumentParser()
+    
+    args = parser.parse_args()
+    _main(args)
