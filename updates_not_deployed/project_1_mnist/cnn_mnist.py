55cf5cdd106d5d777def30fc6d64640c1fb0bcaf
Simon <simonfong6@gmail.com>
2018-07-08 10:48:10 -0700

    Following argparse main convention

--- updates_not_deployed/project_1_mnist/cnn_mnist.py
+++ updates_not_deployed/project_1_mnist/cnn_mnist.py
@@ -50,68 +50,73 @@ import keras
 import numpy as np
 import cv2
 
-# Load MNIST dataset.
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 # Proccess the data from (28,28) to (32,32)
 def procces_image(img):
-	proccesed_image = cv2.resize(img, (32,32))
-	return proccesed_image
+    proccesed_image = cv2.resize(img, (32,32))
+    return proccesed_image
 
-# Resize the images
-x_train = np.array(map(procces_image, x_train))
-x_test = np.array(map(procces_image, x_test))
+def _main(args):
+    # Load MNIST dataset.
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Reshape to fit model
-x_train = np.reshape(x_train,(60000,32,32,1))
-x_test = np.reshape(x_test,(10000,32,32,1))
-print("Resized images to {}".format(x_train.shape))
+    # Resize the images by applying the function to all the images.
+    x_train = np.array(map(procces_image, x_train))
+    x_test = np.array(map(procces_image, x_test))
 
-# One hot encode labels.
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+    # Reshape to fit model
+    x_train = np.reshape(x_train,(60000,32,32,1))
+    x_test = np.reshape(x_test,(10000,32,32,1))
+    print("Resized images to {}".format(x_train.shape))
 
-# Reshape to fit model
-y_train = np.reshape(y_train,(60000,1,1,10))
-y_test = np.reshape(y_test,(10000,1,1,10))
+    # One hot encode labels.
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
 
-# TODO: Currently, sets input dimension to be 784x1. Change to 32x32x1
-inputs = Input(shape=(32,32,1))
+    # Reshape to fit model
+    y_train = np.reshape(y_train,(60000,1,1,10))
+    y_test = np.reshape(y_test,(10000,1,1,10))
 
-x = Conv2D(6,(5,5), activation='sigmoid')(inputs)   # Convolution
-x = MaxPooling2D((2,2),strides=(2,2))(x)            # Pooling
-x = Conv2D(16,(5,5), activation='sigmoid')(x)       # Convolution
-x = MaxPooling2D((2,2),strides=(2,2))(x)            # Pooling
-x = Conv2D(120,(5,5), activation='sigmoid')(x)      # Convolution
-x = Dense(84, activation='tanh')(x)                 # Fully connected layer
-predictions = Dense(10, activation='softmax')(x)    # Inference layer
+    # TODO: Currently, sets input dimension to be 784x1. Change to 32x32x1
+    inputs = Input(shape=(32,32,1))
 
-# This creates a model that includes the Input layer and three Dense layers
-model = Model(inputs=inputs, outputs=predictions)
+    x = Conv2D(6,(5,5), activation='sigmoid')(inputs)   # Convolution
+    x = MaxPooling2D((2,2),strides=(2,2))(x)            # Pooling
+    x = Conv2D(16,(5,5), activation='sigmoid')(x)       # Convolution
+    x = MaxPooling2D((2,2),strides=(2,2))(x)            # Pooling
+    x = Conv2D(120,(5,5), activation='sigmoid')(x)      # Convolution
+    x = Dense(84, activation='tanh')(x)                 # Fully connected layer
+    predictions = Dense(10, activation='softmax')(x)    # Inference layer
 
-# Print model architecture
-model.summary()
+    # This creates a model that includes the Input layer and three Dense layers
+    model = Model(inputs=inputs, outputs=predictions)
 
-# Compile model
-optimizer = keras.optimizers.SGD(lr=1e-4,momentum=0.9)
-model.compile(optimizer,'categorical_crossentropy', metrics=['accuracy'])
+    # Print model architecture
+    model.summary()
 
-# Setting for training.
-NUM_EPOCHS = 40
-BATCH_SIZE = 16
+    # Compile model
+    optimizer = keras.optimizers.SGD(lr=1e-4,momentum=0.9)
+    model.compile(optimizer,'categorical_crossentropy', metrics=['accuracy'])
 
-# Train the model.
-model.fit(x=x_train,y=y_train,batch_size=BATCH_SIZE,epochs=NUM_EPOCHS)
+    # Setting for training.
+    NUM_EPOCHS = 40
+    BATCH_SIZE = 16
 
-# Save the model.
-model.save('yann_mnist.h5')
+    # Train the model.
+    model.fit(x=x_train,y=y_train,batch_size=BATCH_SIZE,epochs=NUM_EPOCHS)
 
-# Test the model.
-metrics = model.evaluate(x=x_test,y=y_test, batch_size=BATCH_SIZE)
+    # Save the model.
+    model.save('yann_mnist.h5')
 
-# Print out the accuracy.
-print("{metrics_names[0]}: {metrics[0]} \n {metrics_names[1]}: {metrics[1]}".format(metrics=metrics, 
-	metrics_names=model.metrics_names))
+    # Test the model.
+    metrics = model.evaluate(x=x_test,y=y_test, batch_size=BATCH_SIZE)
 
+    # Print out the accuracy.
+    print("{metrics_names[0]}: {metrics[0]} \n {metrics_names[1]}: {metrics[1]}".format(metrics=metrics, 
+        metrics_names=model.metrics_names))
 
-
+if(__name__ == '__main__'):
+    import argparse
+    parser = argparse.ArgumentParser()
+    
+    args = parser.parse_args()
+    _main(args)
