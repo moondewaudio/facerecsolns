84c3146e9df57e5d39c3e514d3522b543e07c855
Simon <simonfong6@gmail.com>
2018-07-08 10:49:21 -0700

    Following main

--- updates_not_deployed/project_1_mnist/predict.py
+++ updates_not_deployed/project_1_mnist/predict.py
@@ -15,30 +15,38 @@ import keras
 import numpy as np
 import cv2
 
-# Load MNIST dataset.
-(x_train, y_train), (x_test, y_test) = mnist.load_data()
-
 # Proccess the data from (28,28) to (32,32)
 def procces_image(img):
     proccesed_image = cv2.resize(img, (32,32))
     return proccesed_image
 
-# Resize the images
-x_train = np.array(map(procces_image, x_train))
-x_test = np.array(map(procces_image, x_test))
+def _main(args):
+    # Load MNIST dataset.
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-# Reshape to fit model
-x_train = np.reshape(x_train,(60000,32,32,1))
-x_test = np.reshape(x_test,(10000,32,32,1))
-print("Resized images to {}".format(x_train.shape))
+    # Resize the images
+    x_train = np.array(map(procces_image, x_train))
+    x_test = np.array(map(procces_image, x_test))
 
-# One hot encode labels.
-y_train = to_categorical(y_train)
-y_test = to_categorical(y_test)
+    # Reshape to fit model
+    x_train = np.reshape(x_train,(60000,32,32,1))
+    x_test = np.reshape(x_test,(10000,32,32,1))
+    print("Resized images to {}".format(x_train.shape))
 
-# Reshape to fit model
-y_train = np.reshape(y_train,(60000,1,1,10))
-y_test = np.reshape(y_test,(10000,1,1,10))
+    # One hot encode labels.
+    y_train = to_categorical(y_train)
+    y_test = to_categorical(y_test)
+
+    # Reshape to fit model
+    y_train = np.reshape(y_train,(60000,1,1,10))
+    y_test = np.reshape(y_test,(10000,1,1,10))
+
+if(__name__ == '__main__'):
+    import argparse
+    parser = argparse.ArgumentParser()
+    
+    args = parser.parse_args()
+    _main(args)
 
 
 
