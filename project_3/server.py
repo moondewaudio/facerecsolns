a61eb1796bc88ac0edf1367992464acffbb88a2b
Simon <simonfong6@gmail.com>
2018-07-08 13:55:40 -0700

    Command to activate ssl

--- project_3/server.py
+++ project_3/server.py
@@ -183,7 +183,8 @@ def predict():
     return prediction_json
         
 def main(args):
-    app.run(host='0.0.0.0', port=args.port, threaded=False, debug=args.debug)
+    app.run(host='0.0.0.0', port=args.port, threaded=False, 
+        debug=args.debug, ssl_context=args.ssl)
 
 
 if(__name__ == "__main__"):
@@ -191,5 +192,7 @@ if(__name__ == "__main__"):
     parser = argparse.ArgumentParser()
     parser.add_argument('-p','--port', help="Port that the server will run on.", type=int, default=8080)
     parser.add_argument('-d','--debug', help="Whether or not to run in debug mode.", default=False, action='store_true')
+    parser.add_argument('-s','--ssl', help="Whether or not to run with HTTPS", default=None, action='store_const',const='adhoc')
+
     args = parser.parse_args()
     main(args)
