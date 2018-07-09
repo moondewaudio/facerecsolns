bb525e8077602a61b14644765aaa83e8846bacfc
Simon <simonfong6@gmail.com>
2018-07-09 08:29:05 -0700

    Added todos

--- knn/knn_sklearn.py
+++ knn/knn_sklearn.py
@@ -16,40 +16,30 @@ def _main(args):
     from keras.datasets import mnist
     (x_train, y_train), (x_test, y_test) = mnist.load_data()
     
+    # Proccess dataset
     x_train = np.reshape(x_train,(60000,784))
     x_test = np.reshape(x_test,(10000,784))
     print("Reshaped x data into {}".format(x_train.shape))
 
-    # Create KNN
+    # TODO: Create KNeighorsClassifier with neighbors 3, and n_jobs=-1
     knn = KNeighborsClassifier(n_neighbors=3,n_jobs=-1)
 
 
     # Train with data.
     print("Begin fitting...")
+
+    # TODO: Fit the data
+    # Hint: knn.fit
     knn.fit(x_train, y_train)
     print("Classifier fitted.")
 
     # Evaluate on testing data.
     print("Begin predicting.")
+
+    # TODO: Evaluate on testing data
+    # Hint: knn.score
     acc = knn.score(x_test,y_test)
     print("Accuracy: {}".format(acc))
-
-
-    # Do a test prediction.
-    """
-    tests = 10
-    for i in range(tests):   
-        x_predict = x_test[i]
-        y_predict = knn.predict([x_predict])[0]
-        y_correct = y_test[i]
-        correct = False
-        if(y_predict == y_correct):
-            correct = True
-        message = "Predicted: {predict}, Truth: {truth}, {}".format(correct,
-            predict=y_predict,
-            truth=y_correct)
-        print(message)
-    """
 
   
 if(__name__ == '__main__'):
